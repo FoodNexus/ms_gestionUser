f01d209564886f2809b144deda75cda7706ee790
emna <@mnouchette@@@@*>
2026-03-21 21:37:11 +0100

    creation du projet Spring Boot ms_gestionUser

---
+++ src/main/java/tn/esprit/ms_gestionuser/MsGestionUserApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.ms_gestionuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsGestionUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsGestionUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/ms_gestionuser/MsGestionUserApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.ms_gestionuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsGestionUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
